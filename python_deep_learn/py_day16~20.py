8f2a5241fe87674a1cfdf1b59b101da395376185
heart_angle <1623520966@qq.com>
2019-06-22 23:04:02 +0800

    python学习第十六天到二十天

--- python_deep_learn/py_day16~20.py
+++ python_deep_learn/py_day16~20.py
@@ -16,7 +16,7 @@
 	- 阶乘时间复杂度 - 旅行经销商问题 - NP
 """
 
-# 排序算法(选择，冒泡，归并)和查找算法(顺序和折半)
+"""排序算法(选择，冒泡，归并)和查找算法(顺序和折半)"""
 
 """简单选择排序"""
 '''
@@ -226,6 +226,7 @@ for x in range(20):
 			print(x,y,z)
 '''
 
+'''
 # A、B、C、D、E五人在某天夜里合伙捕鱼 最后疲惫不堪各自睡觉
 # 第二天A第一个醒来 他将鱼分为5份 扔掉多余的1条 拿走自己的一份
 # B第二个醒来 也将鱼分为5份 扔掉多余的1条 拿走自己的一份
@@ -246,3 +247,220 @@ while True:
 		print(fish)
 		break
 	fish += 5
+'''
+
+"""贪婪法例子"""
+"""
+假设小偷有一个背包，最多能装20公斤赃物，他闯入一户人家，发现如下表所示的物品。很显然，
+他不能把所有物品都装入背包，所以确定拿走那些物品，留下哪些
+
+名称	  价格（美元）	重量（kg）
+电脑	  200	        20
+收音机 20	        4
+钟	  175	        10
+花瓶	  50	        2
+书	  10	        1
+油画	  90	        9
+______________________________________________________________________
+贪婪法在求解问题时，总是做出在当前看来最好的选择，不追求最优解，快速找到满意解，。
+"""
+"""
+class Thing(object):
+	# 物品
+	def __init__(self,name,price,weight):
+		self.name = name
+		self.price = price
+		self.weight = weight
+	@property
+	def value(self):
+	# 价格重量比
+		return self.price / self.weight
+
+def input_thing():
+	# 输入物品信息
+	name_str,price_str,weight_str = input().split()
+	return name_str,int(price_str),int(weight_str)
+
+def main():
+	# 主函数
+	max_weight,num_of_things = map(int, input().split())
+	all_things = []
+	for _ in range(num_of_things):
+		all_things.append(Thing(*input_thing()))
+	all_things.sort(key=lambda x: x.value,reverse=True)
+	total_weight = 0
+	total_price = 0
+	for thing in all_things:
+		if total_weight + thing.weight <= max_weight:
+			print(f'小偷拿走了{thing.name}')
+			total_weight += thing.weight
+			total_price += thing.price
+	print(f'总价值:{total_price}美元')
+main()
+"""
+
+"""分治法例子：快速排序"""
+"""
+快速排序 - 选择枢轴元素进行划分，左边都比枢轴小，右边都比枢轴大
+"""
+"""
+def quick_sort(origin_items,comp=lambda x,y: x <= y):
+	items = origin_items[:]
+	_quick_sort(items,0,len(items)-1,comp)
+	return items
+
+def _quick_sort(items,start,end,comp):
+	if start < end:
+		pos = _partition(items,start,end,comp)
+		_quick_sort(items,start,pos-1,comp)
+		_quick_sort(items,pos+1,end,comp)
+
+def _partition(items,start,end,comp):
+	pivot = items[end]
+	i = start -1
+	for j in range(start,end):
+		if comp(items[j],pivot):
+			i += 1
+			items[i],items[j] = items[j],items[i]
+	items[i+1],items[end] = items[end],items[i+1]
+	return i+1
+def main():
+	import random
+	l = []
+	for _ in range(1,1000001):
+		l.append(random.randint(1,1000))
+	res = quick_sort(l)
+	print(res)
+main()
+"""
+
+"""回溯法：骑士巡逻"""
+"""
+递归回溯法：叫试探法，按选优条件向前搜索，当搜索到某一步，发现原来选择并不优或达不到
+"""
+"""
+import sys
+import time
+
+SIZE = 5
+total = 0
+
+def print_board(board):
+	for row in board:
+		for col in row:
+			print(str(col).center(4),end='')
+		print()
+
+def patrol(board,row,col,step=1):
+	if row >= 0 and row < SIZE and col >= 0 and col < SIZE and board[row][col] == 0:
+		board[row][col] = step
+		if step == SIZE*SIZE:
+			global total
+			total += 1
+			print(f'第{total}种走法')
+			print_board(board)
+		patrol(board,row-2,col-2,step+1)
+		patrol(board,row-1,col-2,step+1)
+		patrol(board,row+1,col-2,step+1)
+		patrol(board,row+2,col-1,step+1)
+		patrol(board,row+2,col+1,step+1)
+		patrol(board,row+1,col+2,step+1)
+		patrol(board,row-1,col+2,step+1)
+		patrol(board,row-2,col+1,step+1)
+		board[row][col] = 0
+def main():
+	board = [[0] * SIZE for _ in range(SIZE)]
+	patrol(board,SIZE-1,SIZE-1)
+main()
+"""
+
+"""动态规划例子1：斐波拉契数列(不使用动态规划将会是几何级数复杂度)"""
+"""
+动态规划 - 适用于有重叠子问题和最优子结构性质的问题
+使用动态规划方法所耗费时间往往远少于朴素解法(用空间换时间)
+"""
+"""
+def fib(num,temp={}):
+	# 用递归计算Fibonacci数
+	if num in (1,2):
+		return 1
+	try:
+		return temp[num]
+	except KeyError:
+		temp[num] = fib(num-1) + fib(num-2)
+		return temp[num]
+def main():
+	print(fib(905))
+	print(len(str(fib(750))))
+main()
+"""
+
+"""动态规划例子2：子列表元素之和的最大值(可使用动态规划避免二重循环)"""
+"""
+def main():
+	items = list(map(int,input().split()))
+	size = len(items)
+	overall,partial = {},{}
+	overall[size-1] = partial[size-1] = items[size-1]
+	for i in range(size-2,-1,-1):
+		partial[i] = max(items[i],partial[i+1]+items[i])
+		overall[i] = max(partial[i],overall[i+1])
+	print(overall[0])
+main()
+"""
+
+# 2、函数的使用方法
+"""
+1、将函数视为“一等公民”
+	函数可以赋值给变量
+	函数可以作为函数的参数
+	函数可以作为函数的返回值
+
+2、高阶函数的用法(filter,map以及它们的替代品)
+"""
+"""
+filter() 函数用于过滤序列，过滤掉不符合条件的元素，返回由符合条件元素组成的新列表。
+该接收两个参数，第一个为函数，第二个为序列，序列的每个元素作为参数传递给函数进行判，
+然后返回 True 或 False，最后将返回 True 的元素放到新列表中。
+"""
+"""
+items1 = list(map(lambda x: x ** 2, filter(lambda x:x % 2,range(1,10))))
+print(items1)
+items2 = [x ** 2 for x in range(1,10) if x % 2] # 奇数%2余1，
+print(items2)
+"""
+
+"""
+3、位置参数、可变参数、关键字参数、命名关键字参数
+
+4、参数的元信息(代码可读性问题)
+
+5、匿名函数和内联函数的用法(lambda函数)
+
+6、闭包和作用域问题
+	python中搜索变量的LEGB顺序(Local--> Embeded--> Global--> Built-in)
+	global 和 nonlocal 关键字的作用
+		global:声明或定义全局变量(要么使用现有的全局作用域的变量，要么定义一个变量放到全局作用域)
+		nonllocal：声明使用嵌套作用域的变量(嵌套作用域必须存在该变量，否则报错)
+
+7、装饰器函数(使用装饰器和取消装饰器)
+"""
+# 例子：输出函数执行时间的装饰器
+from functools import wraps
+from time import time
+
+def main(num):
+	while num:
+		print("hello world")
+		num = num - 1
+
+def record_time(func):
+	"""自定义装饰函数的装饰器"""
+	@wraps(func)
+	def wrapper(*args,**kwargs):
+		start = time()
+		result = func(*args,**kwargs)
+		print(f'{func.__name__}:{time()-start}秒')
+		return result
+	return wrapper
+record_time(main(1000))
